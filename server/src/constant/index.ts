d72dbb360bd11fd9a4f13665ed3df3832d5a84eb
victor <xxvnkexin@gmail.com>
2025-04-25 18:38:05 +0800

    feat: add enum

---
+++ server/src/constant/index.ts
@@ -0,0 +1,11 @@
+export enum ErrorCode {
+  ROOM_NOT_FOUND = 10000,
+  INVALID_PASSWORD = 10001,
+  GAME_ALREADY_STARTED = 10002,
+}
+
+export enum ErrorMessage {
+  ROOM_NOT_FOUND = 'Room not found',
+  INVALID_PASSWORD = 'Invalid password',
+  GAME_ALREADY_STARTED = 'Game already started',
+}
---
+++ src/components/Error.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { useGameStore } from '../stores/gameStore';
+const gameStore = useGameStore()
+
+</script>
+
+<template>
+   <div v-show="gameStore.error" class="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50 p-4">
+    <div class="rounded-xl shadow-lg p-6 animate-slide-up  bg-orange-200 text-error-500">
+      <p>{{ gameStore.error }}</p>
+    </div>
+   </div>
+</template>
